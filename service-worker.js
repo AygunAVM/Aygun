be66e6665012ed1742672b1eeb8413be6e4f930a
AygunAVM <f.moonday@gmail.com>
2026-02-26 15:03:28 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,17 @@
-const CACHE_NAME="aygun-cache-v1";
-const urlsToCache=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(response=>response||fetch(e.request)));});
+const CACHE_NAME = "aygun-cache-v1";
+const urlsToCache = [
+  "/Aygun/",
+  "/Aygun/index.html",
+  "/Aygun/data/urunler.json",
+  "/Aygun/data/kullanicilar.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
