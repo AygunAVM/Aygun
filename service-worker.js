bdcb5c37a73ca409c43aef676e53edae5d61c3eb
AygunAVM <f.moonday@gmail.com>
2026-02-27 14:22:01 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,9 +11,7 @@ const urlsToCache = [
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
