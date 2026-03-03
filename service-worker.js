b47a853b8e4738637c94a522cf7b236643cf34bc
AygunAVM <f.moonday@gmail.com>
2026-03-03 15:12:58 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "aygun-v2-cache";
+const CACHE_NAME = "aygun-v3-cache";
 const assets = [
   "./",
   "./index.html",
@@ -10,9 +10,23 @@ const assets = [
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener("fetch", e => {
+  // JSON dosyaları her zaman ağdan çek (güncel fiyatlar için)
+  if (e.request.url.includes('/data/')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
   );
